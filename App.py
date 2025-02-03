c1f5dbcc1c8fa0a462d39bc711d4734a9245902e
killrr <64957160+cnvr76@users.noreply.github.com>
2025-02-03 15:10:35 +0100

    /app: made basic connection with db and web

--- App.py
+++ App.py
@@ -3,26 +3,31 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__, template_folder="./views", static_folder="./public")
 
+query_params = {
+    "newer_than": (10, "day"),
+    "older_than": (1, "day"),
+    "sender": "support@profesia.sk"
+}
+parser = Parser(query_params)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-    return render_template("index_redesign.html")
+    try:
+        data = parser.db.connect().all()
+        # return data
+        return render_template("index_redesign.html", vacancies=data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route("/getAll")
 def get_all():
     try:
-        query_params = {
-            "newer_than": (10, "day"),
-            "older_than": (1, "day"),
-            "sender": "support@profesia.sk"
-        }
-        parser = Parser(query_params)
-        data = parser.db.executeQuery("SELECT * FROM Companies")
+        data = parser.db.connect().executeQuery("SELECT * FROM Vacancies")
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
--- public/scripts/Database.py
+++ public/scripts/Database.py
@@ -1,16 +1,31 @@
 import sqlite3 as sql
 from typing import List, Dict, Tuple
+from pprint import pprint
 
 class Connector:
     def __init__(self):
+        self.connect()
+
+    def connect(self):
         self.connection = sql.connect("db/profesiask.db")
         self.cursor = self.connection.cursor()
+        return self
+    
+    def all(self):
+        query: str = """
+            SELECT v.V_id, v.Position, v.Link, c.Name as 'Company', l.City as 'Location' 
+            FROM Vacancies v
+            JOIN Locations l ON l.L_id = v.Location
+            JOIN Companies c ON c.C_id = v.Company
+        """
+        return self.executeQuery(query)
 
-    def executeQuery(self, query: str) -> List[Tuple] | bool:
+    def executeQuery(self, query: str, params: List=[]) -> List[Dict[str, int | str]] | bool:
         try:
-            self.cursor.execute(query)
+            self.cursor.execute(query, params)
             if self.cursor.description:
-                return self.cursor.fetchall()
+                data = self.cursor.fetchall()
+                return self._format(data, self.cursor.description)
             else:
                 self.connection.commit()
                 print("Query executed succesfully!")
@@ -22,4 +37,11 @@ class Connector:
         except sql.ProgrammingError as pe:
             print(pe)
             return []
-
+        
+    def _format(self, raw_data, description) -> List[Dict[str, int | str]]:
+        columns = [col[0] for col in description]
+        result = []
+        for row in raw_data:
+            formatted = {columns[i]: row[i] for i in range(len(columns))}
+            result.append(formatted)
+        return result
